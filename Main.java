977c2660a5e122d1529a0743c9239f489b0a0c75
dsarotti <sr.sarotti@gmail.com>
2023-01-20 02:20:29 +0100

    necesito dormir;exit

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
-import java.util.Iterator;
-
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("------------------------------------");
+        System.out.println("1º) abecedario completo: ");
+        System.out.println("------------------------------------");
+
         Abecedario abc = new Abecedario();
         for (char x : abc) {
             System.out.println("caracter: " + x);
         }
+        System.out.println("------------------------------------");
+        System.out.println("Fin de la 1º parte.");
+        
         // La salida por consola debería ser:
         // caracter: a
         // caracter: b
@@ -14,33 +20,40 @@ public class Main {
         // caracter: e
         // ...
 
+        System.out.println("====================================");
+        System.out.println("2º) Vocales desde la clase Abecedario: ");
+        System.out.println("------------------------------------");
+
         Abecedario abcb = new Abecedario();
         Abecedario.AbcIterator iter = abcb.iterator();
         while (iter.hasNextVocal()) {
             System.out.println("vocal: " + iter.nextVocal());
         }
+        System.out.println("------------------------------------");
+        System.out.println("Fin de la 2º parte.");
+        
         // La salida por consola debería ser:
         // vocal: a
         // vocal: e
         // vocal: i
         // ...
 
-        // Vocales voc = new Vocales();
-        // for (char x : voc) {
-        //     System.out.println("Vocal: " + x);
-        // }
+        System.out.println("====================================");
+        System.out.println("3º) Vocales desde la subclase \"Vocal\" ");
+        System.out.println("------------------------------------");
+
+        Vocal voc = new Vocal();
+        for (char x : voc) {
+            System.out.println("Vocal: " + x);
+        }
+
+        System.out.println("------------------------------------");
+        System.out.println("Fin de la 3º parte.");
+        System.out.println("------------------------------------");
         // La salida por consola debería ser:
         // Vocal: a
         // Vocal: e
         // Vocal: i
         // ...
-
-        // Abecedario abc = new Abecedario();
-        // for (Character letra : abc) {
-        // System.out.println(letra);
-        // }
-        // for (Character letra : abc) {
-        // System.out.println(itr.nextVocal());
-        // }
     }
 }
---
+++ Vocal.java
@@ -0,0 +1,37 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Vocal extends Abecedario {
+
+    public Vocal() {
+    }
+
+    @Override
+    public VocalIterator iterator(){
+        return new VocalIterator();       
+    }
+
+    class VocalIterator extends AbcIterator{
+        int pos = 0;
+
+        @Override
+        public boolean hasNext() {
+            return pos < vocPos[vocPos.length - 1];
+        }
+
+        @Override
+        public Character next() throws NoSuchElementException  {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            } else if (pos!=0){
+                for (int i = 0; i < vocPos.length; i++) {
+                    if (pos < vocPos[i]) {
+                        pos = vocPos[i];
+                        break;
+                    }
+                }
+            }
+            return letras[pos++];
+        }
+    }
+}
