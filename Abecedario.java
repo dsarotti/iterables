b0336b8658d3e588d8edb2a7341dd182db2f758f
dsarotti <sr.sarotti@gmail.com>
2023-01-20 01:17:24 +0100

    Funciona!

--- Abecedario.java
+++ Abecedario.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -6,15 +5,14 @@ public class Abecedario implements Iterable<Character> {
     private final Character[] letras = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
     private final int[] vocPos = { 0, 4, 8, 14, 20 };
-    private ArrayList<String> hola;
-    
+
     @Override
     public AbcIterator iterator() {
         return new AbcIterator();
     }
 
     class AbcIterator implements Iterator<Character> {
-        int pos=0;
+        int pos = 0;
 
         @Override
         public boolean hasNext() throws NoSuchElementException {
@@ -31,16 +29,21 @@ public class Abecedario implements Iterable<Character> {
         }
 
         public boolean hasNextVocal() {
-            return pos < vocPos[4];
+            return pos < vocPos[vocPos.length - 1];
         }
 
         public Character nextVocal() throws NoSuchElementException {
             if (!hasNextVocal()) {
                 throw new NoSuchElementException();
             } else {
+                for (int i = 0; i < vocPos.length; i++) {
+                    if (pos <= vocPos[i]) {
+                        pos = vocPos[i];
+                        break;
+                    }
+                }
             }
-            return null;
+            return letras[pos++];
         }
-
     }
 }
